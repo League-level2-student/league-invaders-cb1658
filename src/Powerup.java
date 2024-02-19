4519c1389883be443100cd6aaf8e94f8fd6e5118
tweet <tweet@DESKTOP-87MD9CS.attlocal.net>
2024-02-18 16:32:21 -0800

    v1.2-pre2b

--- src/Powerup.java
+++ src/Powerup.java
@@ -1,10 +1,11 @@
+import java.util.Random;
 
 public class Powerup extends Projectile{
 
 	public Powerup(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		
-		this.speed = 5;
+		this.speed = new Random().nextInt(50)+1;
 	}
 
 }
